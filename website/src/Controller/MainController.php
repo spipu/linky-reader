e9ac9d6fb572f3687fbf9286a3b4915cca684f2d
spipu <spipu45@gmail.com>
2020-04-13 16:20:31 +0200

    add log on homepage

--- website/src/Controller/MainController.php
+++ website/src/Controller/MainController.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,10 +16,24 @@ class MainController extends AbstractController
 {
     /**
      * @Route("/", name="app_home", methods="GET")
+     * @param ContainerInterface $container
      * @return Response
      */
-    public function home(): Response
+    public function home(ContainerInterface $container): Response
     {
-        return $this->render('main/home.html.twig');
+        $logDir = $container->getParameter('kernel.logs_dir');
+
+        $filename = $logDir .DIRECTORY_SEPARATOR . 'cron.log';
+        $content = sprintf('Log File [%s] is missing', $filename);
+        if (is_file($filename)) {
+            $content = file_get_contents($filename);
+        }
+
+        return $this->render(
+            'main/home.html.twig',
+            [
+                'content' => $content,
+            ]
+        );
     }
 }
