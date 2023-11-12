f81088afa5c7692cb722c0a278eb3b44f37f38b5
spipu <spipu45@gmail.com>
2023-11-12 01:08:24 +0100

    add update script

--- architecture/update-app.sh
+++ architecture/update-app.sh
@@ -2,18 +2,38 @@
 
 FOLDER="/var/www/linky-reader"
 
+echo ""
+echo "==[UPDATE FROM GIT]=="
+echo ""
+
 cd "$FOLDER"
 git pull
+
+echo ""
+echo "==[CLEAN FILES]=="
+echo ""
 
 sudo -u www-data rm -rf "$FOLDER/website/war/cache"
 sudo -u www-data rm -rf "$FOLDER/website/var/log"
 rm -rf "$FOLDER/website/var/cache"
 rm -rf "$FOLDER/website/var/log"
 
+echo ""
+echo "==[COMPOSER UPDATE]=="
+echo ""
+
 cd "$FOLDER/website"
 composer install
+
+echo ""
+echo "==[CLEAN FILES]=="
+echo ""
 
 rm -rf "$FOLDER/website/var/cache"
 rm -rf "$FOLDER/website/var/log"
 sudo -u www-data rm -rf "$FOLDER/website/var/cache"
 sudo -u www-data rm -rf "$FOLDER/website/var/log"
+
+echo ""
+echo "==[FINISHED]=="
+echo ""
